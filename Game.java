65e256fcbc21d2041bd59d347867dec85362770f
Ali Sartaz Khan <71156712+alisartazkhan@users.noreply.github.com>
2022-10-02 23:42:46 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -21,8 +21,6 @@ public class Game {
 	
 	public static void main(String args[]) 
 	{
-		
-		
 		int playerNumber;
 		playerNumber = 2; // set player number
 		ArrayList<Player> playerList = new ArrayList<>();
